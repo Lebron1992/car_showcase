c9e39855812fd4b0265f64d504c900dbf204a4cc
Lebron1992 <wenzhi.zeng@outlook.com>
2023-08-04 11:24:25 +0800

    implemenet CarCard

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,21 +1,35 @@
 'use client';
 
 import { CustomButtonProps } from '@/types';
+import Image from 'next/image';
 
 const CustomButton = ({
   title,
   containerStyle,
   type,
-  handleClick
+  handleClick,
+  textStyle,
+  rightIcon,
+  isDisabled
 }: CustomButtonProps) => {
   return (
     <button
-      disabled={false}
+      disabled={isDisabled || false}
       type={type || 'button'}
       className={`custom-btn ${containerStyle}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyle}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 };
